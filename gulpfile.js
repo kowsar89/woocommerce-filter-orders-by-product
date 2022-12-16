5a597a10a7d917dbc8fe9596ccba3c1a723fc897
Md Kowsar Hossain <kowsar9989@gmail.com>
2022-12-16 10:34:31 +0600

    Improved

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,14 @@
 const project         = require('./package.json');;
 const gulp            = require('gulp');
 const wpPot           = require('gulp-wp-pot');
-const clean           = require('gulp-clean');
-const zip             = require('gulp-zip');
 
 gulp.task('pot', function () {
 	return gulp.src(['**/*.php', '!__*/**', '!src/**', '!assets/**'])
 	.pipe(wpPot( {
 		domain: project.name,
+		includePOTCreationDate: false,
 		bugReport: 'contact@kowsarhossain.com',
 		team: 'KowsarHossain <contact@kowsarhossain.com>'
 	} ))
 	.pipe(gulp.dest('languages/'+project.name+'.pot'));
 });
-
-gulp.task('clean', function () {
-	return gulp.src('__build/*.*', {read: false})
-	.pipe(clean());
-});
-
-gulp.task('zip', function () {
-	return gulp.src(['**', '!__*/**', '!node_modules/**', '!src/**', '!gulpfile.js', '!readme.md', '!package.json', '!package-lock.json', '!todo.txt'], { base: '..' })
-	.pipe(zip(project.name+'.zip'))
-	.pipe(gulp.dest('__build'))
-});
-
-gulp.task('build', gulp.series('pot','clean','zip'));
